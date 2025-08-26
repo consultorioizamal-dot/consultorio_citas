db56291d284f0532511b64e45c0baba4044a6665
consultorioizamal-dot <consultorioizamal@gmail.com>
2025-08-26 10:17:50 -0600

    Update apps.py

--- citas/apps.py
+++ citas/apps.py
@@ -1,15 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
 
 class CitasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "citas"
-
-    def ready(self):
-        username = "Monica"
-        password = "ConsultorioIza"
-        email = "consultorioizamal@gmail.com"
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
-            print("✅ Superusuario creado con éxito")
