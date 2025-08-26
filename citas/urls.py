501c4303bcf889282249b53a41b82628e2a62623
consultorioizamal-dot <consultorioizamal@gmail.com>
2025-08-26 01:08:23 -0600

    Update urls.py

--- citas/urls.py
+++ citas/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,8 +11,4 @@ urlpatterns = [
     path('exportar-excel/', views.exportar_excel, name='exportar_excel'),
     path('exportar-pdf/', views.exportar_pdf, name='exportar_pdf'),
     path('api/horas_disponibles/', views.api_horas_disponibles, name='api_horas_disponibles'),
-
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
-    path("register/", views.register, name="register"),
 ]
