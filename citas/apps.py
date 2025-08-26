47d7d8316e1500f6ade7e1da4a77822d34510c95
consultorioizamal-dot <consultorioizamal@gmail.com>
2025-08-26 09:12:55 -0600

    Add files via upload

--- citas/apps.py
+++ citas/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import User
 
 class CitasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'citas'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "citas"
+
+    def ready(self):
+        username = "Monica"
+        password = "ConsultorioIza"
+        email = "consultorioizamal@gmail.com"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            print("✅ Superusuario creado con éxito")
